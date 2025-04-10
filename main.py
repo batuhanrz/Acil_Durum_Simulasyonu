3d7ec00b26d501bd363d3332370553472cc2763c
batuhanrz <164503865+batuhanrz@users.noreply.github.com>
2025-04-10 18:37:35 +0300

    GUI'yı ayarlama

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pytest
+from GUI.main_window import MainWindow
 
 def init_tests():
     """Tüm testleri çalıştırır"""
     print("[MAIN] Test sistemi başlatılıyor...")
-    pytest.main(["Tests"])  # Tests klasöründeki tüm testleri çalıştırır
+    # pytest.main(["Tests"])  # Tests klasöründeki tüm testleri çalıştırır
+
+def init_gui():
+    """CustomTkinter GUI'yi başlatır"""
+    print("[MAIN] GUI başlatılıyor...")
+    app = MainWindow()
+    app.mainloop()
 
 if __name__ == "__main__":
-    init_tests()   #
+    # init_tests()   # Testleri çalıştırmak istersen yorumdan çıkar
+    init_gui()       # GUI'yi başlat
