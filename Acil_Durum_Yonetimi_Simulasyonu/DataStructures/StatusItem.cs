5bb582c4f2e78f8fe0b571884a12be68a04c1c91
batuhanrz <164503865+batuhanrz@users.noreply.github.com>
2025-05-11 18:45:11 +0300

    Durum nesnesi

---
+++ Acil_Durum_Yonetimi_Simulasyonu/DataStructures/StatusItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acil_Durum_Yonetimi_Simulasyonu.DataStructures
+{
+    public class StatusItem
+    {
+        public string Name { get; set; }
+        public List<string> DataItems { get; set; }
+
+        public StatusItem(string name)
+        {
+            Name = name;
+            DataItems = new List<string>();
+        }
+        public void AddData(string data)
+        {
+            DataItems.Add(data);
+        }
+        public string GetFormattedData()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"--- {Name} ---");
+            foreach (string data in DataItems)
+            {
+                sb.AppendLine(data);
+            }
+            return sb.ToString();
+        }
+    }
+}
